efb09fd2d504b239fb61b35305beacb8df1aeacb
Unknown <tgraff13@stundent.aau.dk>
2018-03-12 14:06:45 +0100

    change setup

--- R/data/RPI/setup_data.R
+++ R/data/RPI/setup_data.R
@@ -7,9 +7,11 @@ data = slide(data, Var = 'RPI', NewVar = 'lag_RPI', slideBy = -1)  #lag værdi a
 data = na.omit(data)
 
 idx =  floor(0.80 * nrow(data)) 
+
+
+
 data_train = data[1:idx,]
 data_test = data[-c(1:idx),]
-
 
 y = scale(as.matrix(data_train[,"RPI"]))  %>% na.omit() 
 #y = y[-1] #fjerner en enkelt observation mere
@@ -17,4 +19,8 @@ drops <- c("RPI")
 data_train = data_train[ , !(names(data_train) %in% drops)] #FJERNER RPI
 X = scale(as.matrix(data_train[, -c(1,2)]))  %>% na.omit()
 
+#fjerner dato og X og scallerer
+data_train = data_train[, -c(1,2)]
+data_train = scale(as.matrix(data_train))
 
+                     

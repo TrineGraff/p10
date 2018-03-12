94e7bc881f8b04865752b27a64d1a5c565c7103b
Unknown <tgraff13@stundent.aau.dk>
2018-03-12 15:50:11 +0100

    add setup_data

--- R/data/RPI/setup_data.R
+++ R/data/RPI/setup_data.R
@@ -3,24 +3,14 @@ library(tidyverse)
 library(DataCombine)
 
 data = read.csv("transformed_data.csv") %>% as.data.frame()
-data = slide(data, Var = 'RPI', NewVar = 'lag_RPI', slideBy = -1)  #lag værdi af responsen
 data = na.omit(data)
 
 idx =  floor(0.80 * nrow(data)) 
-
-
-
 data_train = data[1:idx,]
+train_dato = data_train$dato
+data_train = scale(as.matrix(data_train[, -c(1, 2)]))
+
 data_test = data[-c(1:idx),]
+test_dato = data_test$dato
+data_test = scale(as.matrix(data_test[, -c(1, 2)]))
 
-y = scale(as.matrix(data_train[,"RPI"]))  %>% na.omit() 
-#y = y[-1] #fjerner en enkelt observation mere
-drops <- c("RPI")
-data_train = data_train[ , !(names(data_train) %in% drops)] #FJERNER RPI
-X = scale(as.matrix(data_train[, -c(1,2)]))  %>% na.omit()
-
-#fjerner dato og X og scallerer
-data_train = data_train[, -c(1,2)]
-data_train = scale(as.matrix(data_train))
-
-                     

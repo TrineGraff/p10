b4b4adab25dceebecfb7280a014bbd741230a478
LouiseNygaardChristensen <lnch13@student.aau.dk>
2018-03-13 14:37:06 +0100

    updated data

--- R/data/transf_data.R
+++ R/data/transf_data.R
@@ -1,15 +1,13 @@
 library(foreach)
 library(tidyverse)
-library(ggplot2)library(ggplot2)
-library(gridExtra)
+library(ggplot2)
 library(gridExtra)
 
-date <- read.csv("/Users/LouiseNygaardChristensen/Desktop/P10/R/data/FRED_MD/current.csv", sep = ",",
-                 header = TRUE)[-1, 1]
-rawdata <- read.csv("/Users/LouiseNygaardChristensen/Desktop/P10/R/data/FRED_MD/current.csv", sep = ",",
-                    header = TRUE)[-1, -1]
-tcodes <- as.numeric(read.csv("/Users/LouiseNygaardChristensen/Desktop/P10/R/data/FRED_MD/current.csv", sep = ",",
-                   header = TRUE)[1, -1])
+FRED_MD <- read.csv("/Users/LouiseNygaardChristensen/Desktop/P10/R/data/FRED_MD/current.csv", sep = ",",
+                    header = TRUE)
+date <- as.Date(FRED_MD[-1, 1], format = "%m/%d/%Y")
+rawdata <- FRED_MD[-1, -1]
+tcodes <- as.numeric(FRED_MD[1, -1])
 
 
 # Transformation af data --------------------------------------------------
@@ -74,69 +72,66 @@ LocateNAs(transformed_data)
 
 # vi vælger at fjerne følgende kolonner: 58, 60, 95, 123, 128
 # som svarer til variablene ACOGNO, ANDENOx, TWEXMMTH, UMCSENTx, VXOCLSx
-# Yderligere fjernes de første 12 obs samt de sidste 4 obs (pga transformationerne)
+# Yderligere fjernes de første 12 obs samt de sidste 5 obs (pga transformationerne)
 # s.a. data starter fra d. 1 jan 1960 til 1 juli 2017
-# Vi får altså et datasæt med 691 obs og 124 variable
+# Vi får altså et datasæt med 691 obs og 123 variable
 
 data <- transf_data[-c(1:12, 704:708), -c(58, 60, 95, 123, 128)]
 LocateNAs(data)
-dato <- as.Date(date[-c(1:12, 704:708)], format = "%m/%d/%Y")
+dato <- date[-c(1:12, 704:708)]
 
-# ikke-stationær
-data_nons <- data.frame(dato, rawdata[-c(1:12, 704:708), -c(58, 60, 95, 123, 128)])
+df <- data.frame(dato, data)
+write.csv(df, file = "transformed_data.csv")
 
-rpi_nons <- ggplot(data_nons, aes(dato, RPI)) + geom_line() +
+rpi_s <- ggplot(df, aes(dato, RPI)) + geom_line() +
   labs(x = " ", y = "?") + ggtitle("RPI")
-unrate_nons <- ggplot(data_nons, aes(dato, UNRATE)) + geom_line() +
+unrate_s <- ggplot(df, aes(dato, UNRATE)) + geom_line() +
   labs(x = " ", y = "?") + ggtitle("UNRATE")
-m1sl_nons <- ggplot(data_nons, aes(dato, M1SL)) + geom_line() +
+m1sl_s <- ggplot(df, aes(dato, M1SL)) + geom_line() +
   labs(x = " ", y = "?") + ggtitle("M1SL")
-m2sl_nons <- ggplot(data_nons, aes(dato, M2SL)) + geom_line() +
+m2sl_s <- ggplot(df, aes(dato, M2SL)) + geom_line() +
   labs(x = " ", y = "?") + ggtitle("M2SL")
-fedfunds_nons <- ggplot(data_nons, aes(dato, FEDFUNDS)) + geom_line() +
+fedfunds_s <- ggplot(df, aes(dato, FEDFUNDS)) + geom_line() +
   labs(x = " ", y = "?") + ggtitle("FEDFUNDS")
-tb3ms_nons <- ggplot(data_nons, aes(dato, TB3MS)) + geom_line() +
+tb3ms_s <- ggplot(df, aes(dato, TB3MS)) + geom_line() +
   labs(x = " ", y = "?") + ggtitle("TB3MS")
-gs5_nons <- ggplot(data_nons, aes(dato, GS5)) + geom_line() +
+gs5_s <- ggplot(df, aes(dato, GS5)) + geom_line() +
   labs(x = " ", y = "?") + ggtitle("GS5")
-exjpusx_nons <- ggplot(data_nons, aes(dato, EXJPUSx)) + geom_line() +
+exjpusx_s <- ggplot(df, aes(dato, EXJPUSx)) + geom_line() +
   labs(x = " ", y = "?") + ggtitle("EXJPUSx")
-cpiaucsl_nons <- ggplot(data_nons, aes(dato, CPIAUCSL)) + geom_line() +
+cpiaucsl_s <- ggplot(df, aes(dato, CPIAUCSL)) + geom_line() +
   labs(x = " ", y = "?") + ggtitle("CPIAUCSL")
-sp500_nons <- ggplot(data_nons, aes(dato, S.P.500)) + geom_line() +
-  labs(x = " ", y = "?") + ggtitle("S.P.500")
-
-grid.arrange(rpi_nons, unrate_nons, m1sl_nons, m2sl_nons, fedfunds_nons, 
-             tb3ms_nons, gs5_nons, exjpusx_nons, cpiaucsl_nons, sp500_nons, ncol=2) 
-
-
-# stationær
-data_s <- data.frame(dato, data)
-write.csv(data_s, file = "transformed_data.csv")
-
-rpi_s <- ggplot(data_s, aes(dato, RPI)) + geom_line() +
-  labs(x = " ", y = "?") + ggtitle("RPI")
-unrate_s <- ggplot(data_s, aes(dato, UNRATE)) + geom_line() +
-  labs(x = " ", y = "?") + ggtitle("UNRATE")
-m1sl_s <- ggplot(data_s, aes(dato, M1SL)) + geom_line() +
-  labs(x = " ", y = "?") + ggtitle("M1SL")
-m2sl_s <- ggplot(data_s, aes(dato, M2SL)) + geom_line() +
-  labs(x = " ", y = "?") + ggtitle("M2SL")
-fedfunds_s <- ggplot(data_s, aes(dato, FEDFUNDS)) + geom_line() +
-  labs(x = " ", y = "?") + ggtitle("FEDFUNDS")
-tb3ms_s <- ggplot(data_s, aes(dato, TB3MS)) + geom_line() +
-  labs(x = " ", y = "?") + ggtitle("TB3MS")
-gs5_s <- ggplot(data_s, aes(dato, GS5)) + geom_line() +
-  labs(x = " ", y = "?") + ggtitle("GS5")
-exjpusx_s <- ggplot(data_s, aes(dato, EXJPUSx)) + geom_line() +
-  labs(x = " ", y = "?") + ggtitle("EXJPUSx")
-cpiaucsl_s <- ggplot(data_s, aes(dato, CPIAUCSL)) + geom_line() +
-  labs(x = " ", y = "?") + ggtitle("CPIAUCSL")
-sp500_s <- ggplot(data_s, aes(dato, S.P.500)) + geom_line() +
+sp500_s <- ggplot(df, aes(dato, S.P.500)) + geom_line() +
   labs(x = " ", y = "?") + ggtitle("S.P.500")
 
 grid.arrange(rpi_s, unrate_s, m1sl_s, m2sl_s, fedfunds_s, 
              tb3ms_s, gs5_s, exjpusx_s, cpiaucsl_s, sp500_s, ncol=2) 
 
 
+# Ingen transformation
+data_nons <- data.frame(date, rawdata)
+
+rpi_nons <- ggplot(data_nons, aes(date, RPI)) + geom_line() +
+  labs(x = " ", y = "?") + ggtitle("RPI")
+unrate_nons <- ggplot(data_nons, aes(date, UNRATE)) + geom_line() +
+  labs(x = " ", y = "?") + ggtitle("UNRATE")
+m1sl_nons <- ggplot(data_nons, aes(date, M1SL)) + geom_line() +
+  labs(x = " ", y = "?") + ggtitle("M1SL")
+m2sl_nons <- ggplot(data_nons, aes(date, M2SL)) + geom_line() +
+  labs(x = " ", y = "?") + ggtitle("M2SL")
+fedfunds_nons <- ggplot(data_nons, aes(date, FEDFUNDS)) + geom_line() +
+  labs(x = " ", y = "?") + ggtitle("FEDFUNDS")
+tb3ms_nons <- ggplot(data_nons, aes(date, TB3MS)) + geom_line() +
+  labs(x = " ", y = "?") + ggtitle("TB3MS")
+gs5_nons <- ggplot(data_nons, aes(date, GS5)) + geom_line() +
+  labs(x = " ", y = "?") + ggtitle("GS5")
+exjpusx_nons <- ggplot(data_nons, aes(date, EXJPUSx)) + geom_line() +
+  labs(x = " ", y = "?") + ggtitle("EXJPUSx")
+cpiaucsl_nons <- ggplot(data_nons, aes(date, CPIAUCSL)) + geom_line() +
+  labs(x = " ", y = "?") + ggtitle("CPIAUCSL")
+sp500_nons <- ggplot(data_nons, aes(date, S.P.500)) + geom_line() +
+  labs(x = " ", y = "?") + ggtitle("S.P.500")
+
+grid.arrange(rpi_nons, unrate_nons, m1sl_nons, m2sl_nons, fedfunds_nons, 
+             tb3ms_nons, gs5_nons, exjpusx_nons, cpiaucsl_nons, sp500_nons, ncol=2) 
 

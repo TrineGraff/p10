70b822d2ba6c4d83b31110ad8265962d6383d13d
LouiseNygaardChristensen <lnch13@student.aau.dk>
2018-06-08 01:20:19 +0200

    changes

--- R/unrate/Vurdering/dmtest.R
+++ R/unrate/Vurdering/dmtest.R
@@ -22,20 +22,28 @@ adap_ols_bic = read.csv(file = "results/fc_lasso_ols_bic.csv")
 adap_lasso_kryds = read.csv(file = "results/fc_adap_lasso_kryds.csv")
 adap_lasso_bic = read.csv(file = "results/fc_adap_lasso_bic.csv")
 
+lasso_TG_kryds = read.csv(file = "results/fc_lasso_TG_kryds.csv")
+lasso_TG_bic = read.csv(file = "results/fc_lasso_TG_bic.csv")
+
 lars_kryds = read.csv(file = "results/fc_lars_kryds.csv")
 lars_bic = read.csv(file = "results/fc_lars_bic.csv")
 
 lars_lasso_kryds = read.csv(file = "results/fc_lars_lasso_kryds.csv")
 lars_lasso_bic = read.csv(file = "results/fc_lars_lasso_bic.csv")
 
+LARS_TG_kryds = read.csv(file = "results/fc_lars_TG_kryds.csv")
+LARS_TG_bic = read.csv(file = "results/fc_lars_TG_bic.csv")
+
 diffs <- data.frame(ar4[,2] - y.test, faktor_ic1[,2] - y.test, faktor_ic2[,2] - y.test, 
                     faktor_ic3[,2] - y.test, lasso_kryds[,2] - y.test, lasso_bic[,2] - y.test, 
                     ridge_kryds[,2] - y.test, ridge_bic[,2] - y.test,
                     gglasso_kryds[,2] - y.test, gglasso_bic[,2] - y.test, 
                     adap_ols_kryds[,2] - y.test, adap_ols_bic[,2] - y.test,
-                    adap_lasso_kryds[,2] - y.test, adap_lasso_bic[,2] - y.test, 
+                    adap_lasso_kryds[,2] - y.test, adap_lasso_bic[,2] - y.test,
+                    lasso_TG_kryds[,2] - y.test, lasso_TG_bic[,2] - y.test,
                     lars_kryds[,2] - y.test, lars_bic[,2] - y.test,
-                    lars_lasso_kryds[,2] - y.test, lars_lasso_bic[,2] - y.test)
+                    lars_lasso_kryds[,2] - y.test, lars_lasso_bic[,2] - y.test,
+                    LARS_TG_kryds[,2] - y.test, LARS_TG_bic[,2] - y.test)
 
 loss <- list(
   abs = abs(diffs),
@@ -68,11 +76,17 @@ dm.test(loss$abs$faktor_ic2...2....y.test, loss$abs$adap_ols_bic...2....y.test, 
 dm.test(loss$abs$faktor_ic2...2....y.test, loss$abs$adap_lasso_kryds...2....y.test, alternative = "two.sided", h = 1, power = 1)
 dm.test(loss$abs$faktor_ic2...2....y.test, loss$abs$adap_lasso_bic...2....y.test, alternative = "two.sided", h = 1, power = 1)
 
+dm.test(loss$abs$faktor_ic2...2....y.test, loss$abs$lasso_TG_kryds...2....y.test, alternative = "two.sided", h = 1, power = 1)
+dm.test(loss$abs$faktor_ic2...2....y.test, loss$abs$lasso_TG_bic...2....y.test, alternative = "two.sided", h = 1, power = 1)
+
 dm.test(loss$abs$faktor_ic2...2....y.test, loss$abs$lars_kryds...2....y.test, alternative = "two.sided", h = 1, power = 1)
 dm.test(loss$abs$faktor_ic2...2....y.test, loss$abs$lars_bic...2....y.test, alternative = "two.sided", h = 1, power = 1)
 
 dm.test(loss$abs$faktor_ic2...2....y.test, loss$abs$lars_lasso_kryds...2....y.test, alternative = "two.sided", h = 1, power = 1)
 dm.test(loss$abs$faktor_ic2...2....y.test, loss$abs$lars_lasso_bic...2....y.test, alternative = "two.sided", h = 1, power = 1)
+
+dm.test(loss$abs$faktor_ic2...2....y.test, loss$abs$LARS_TG_kryds...2....y.test, alternative = "two.sided", h = 1, power = 1)
+dm.test(loss$abs$faktor_ic2...2....y.test, loss$abs$LARS_TG_bic...2....y.test, alternative = "two.sided", h = 1, power = 1)
 
 ### sq
 # AR
@@ -96,13 +110,17 @@ dm.test(loss$sq$faktor_ic2...2....y.test, loss$sq$adap_ols_bic...2....y.test, al
 dm.test(loss$sq$faktor_ic2...2....y.test, loss$sq$adap_lasso_kryds...2....y.test, alternative = "two.sided", h = 1, power = 2)
 dm.test(loss$sq$faktor_ic2...2....y.test, loss$sq$adap_lasso_bic...2....y.test, alternative = "two.sided", h = 1, power = 2)
 
+dm.test(loss$abs$faktor_ic2...2....y.test, loss$sq$lasso_TG_kryds...2....y.test, alternative = "two.sided", h = 1, power = 1)
+dm.test(loss$abs$faktor_ic2...2....y.test, loss$sq$lasso_TG_bic...2....y.test, alternative = "two.sided", h = 1, power = 1)
+
 dm.test(loss$sq$faktor_ic2...2....y.test, loss$sq$lars_kryds...2....y.test, alternative = "two.sided", h = 1, power = 2)
 dm.test(loss$sq$faktor_ic2...2....y.test, loss$sq$lars_bic...2....y.test, alternative = "two.sided", h = 1, power = 2)
 
 dm.test(loss$sq$faktor_ic2...2....y.test, loss$sq$lars_lasso_kryds...2....y.test, alternative = "two.sided", h = 1, power = 2)
 dm.test(loss$sq$faktor_ic2...2....y.test, loss$sq$lars_lasso_bic...2....y.test, alternative = "two.sided", h = 1, power = 2)
 
-
+dm.test(loss$sq$faktor_ic2...2....y.test, loss$abs$LARS_TG_kryds...2....y.test, alternative = "two.sided", h = 1, power = 1)
+dm.test(loss$sq$faktor_ic2...2....y.test, loss$abs$LARS_TG_bic...2....y.test, alternative = "two.sided", h = 1, power = 1)
 
 
 
